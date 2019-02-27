7202a740773c2ab1507bf058b3e116a54be23f4b
Gary Bernstein <garyb@physics.upenn.edu>
2019-02-27 10:59:11 -0500

    Fix some imports in __init.py__

--- gbutil/decam/__init__.py
+++ gbutil/decam/__init__.py
@@ -3,9 +3,9 @@ DECam-specific information and tasks
 """
 
 # Put utilities into current namespace
-from decamUtils import plotColors,amps,dps,overscanColumns,centralAmpPix,allAmpPix
-from decamUtils import ccdnums, ccdCorners, ccdBounds, ccdCenters, minimalHeader
-from decamUtils import XtalkRemover
-from decamTrig import parallactic, dcr, recenter
-from epochFinder import mjdOfEpoch, EpochFinder
+from . decamUtils import plotColors,amps,dps,overscanColumns,centralAmpPix,allAmpPix
+from . decamUtils import ccdnums, ccdCorners, ccdBounds, ccdCenters, minimalHeader
+from . decamUtils import XtalkRemover
+from . decamTrig import parallactic, dcr, recenter
+from . epochFinder import mjdOfEpoch, EpochFinder
 
